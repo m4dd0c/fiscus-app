891b06ab9ec2383235f670a18fafeab5756a5432
Manish Suthar <manishsuthar078@gmail.com>
2025-03-01 15:45:20 +0530

    impr: better comment

--- src/app/api/plaid/exchange-token/route.ts
+++ src/app/api/plaid/exchange-token/route.ts
@@ -5,18 +5,20 @@ import { auth, clerkClient } from "@clerk/nextjs/server";
 export async function POST(req: Request) {
   try {
     const { public_token } = await req.json();
-    const { userId } = await auth(); // Get Clerk user ID
+    // getting clerk user id
+    const { userId } = await auth();
 
     if (!userId) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
-    // Exchange public_token for access_token
+    // exchange public_token for access_token
     const response = await client.itemPublicTokenExchange({ public_token });
     const access_token = response.data.access_token;
 
     const clerk = await clerkClient();
 
-    // Store access_token in Clerk user metadata
+    // store access_token in clerk user metadata, since we don't have db at the moment.
+    // publicMetadata can only store upto 8kb
     await clerk.users.updateUserMetadata(userId, {
       publicMetadata: {
         access_token,
