4386f083ed019ceac5e13617b4d9f3f8639f59d1
Manish Suthar <manishsuthar078@gmail.com>
2025-03-01 15:46:03 +0530

    impr: better comment

--- src/app/api/plaid/transactions/route.ts
+++ src/app/api/plaid/transactions/route.ts
@@ -2,12 +2,12 @@ import { NextResponse } from "next/server";
 import { currentUser } from "@clerk/nextjs/server";
 import { client } from "@/lib/service/Plaid";
 
-const START_DATE = "2024-01-01"; // Adjust as needed
+const START_DATE = "2024-01-01";
 const END_DATE = new Date().toISOString().split("T")[0];
 
 export async function GET() {
   try {
-    // Get current user details
+    // get current user details
     const user = await currentUser();
     if (!user)
       return NextResponse.json({ error: "User not found" }, { status: 404 });
@@ -20,7 +20,7 @@ export async function GET() {
       );
     }
 
-    // Fetch transactions from Plaid
+    // fetch transactions from plaid
     const response = await client.transactionsGet({
       access_token,
       start_date: START_DATE,
