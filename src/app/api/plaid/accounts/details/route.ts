54696fdc2124b47ded412536407aa7007a2ac8ad
Manish Suthar <manishsuthar078@gmail.com>
2025-03-01 15:43:49 +0530

    refactor: space

--- src/app/api/plaid/accounts/details/route.ts
+++ src/app/api/plaid/accounts/details/route.ts
@@ -3,26 +3,27 @@ import { client } from "@/lib/service/Plaid";
 import { currentUser } from "@clerk/nextjs/server";
 
 export async function GET() {
- try {
-  const user = await currentUser();
-  if (!user)
-   return NextResponse.json({ error: "User not found" }, { status: 404 });
+  try {
+    const user = await currentUser();
+    if (!user)
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
 
-  const access_token = user.publicMetadata.access_token as string;
-  if (!access_token) {
-   return NextResponse.json(
-    { error: "No access token found" },
-    { status: 400 }
-   );
+    const access_token = user.publicMetadata.access_token as string;
+    if (!access_token) {
+      return NextResponse.json(
+        { error: "No access token found" },
+        { status: 400 },
+      );
+    }
+    // getting detailed accounts
+    const response = await client.authGet({ access_token });
+    const { accounts, numbers, request_id } = response.data;
+
+    return NextResponse.json({ accounts, numbers, request_id });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error generating link token", stack: error },
+      { status: 500 },
+    );
   }
-  const response = await client.authGet({ access_token });
-  const { accounts, numbers, request_id } = response.data;
-
-  return NextResponse.json({ accounts, numbers, request_id });
- } catch (error) {
-  return NextResponse.json(
-   { error: "Error generating link token", stack: error },
-   { status: 500 }
-  );
- }
 }
