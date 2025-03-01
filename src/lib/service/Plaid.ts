420a5cd0c54a0c91387294d51a1c4114c60d2b92
Manish Suthar <manishsuthar078@gmail.com>
2025-03-01 15:38:28 +0530

    chore: cmment added

--- src/lib/service/Plaid.ts
+++ src/lib/service/Plaid.ts
@@ -1,34 +1,35 @@
 import { Configuration, PlaidApi, PlaidEnvironments } from "plaid";
 
 export const config = new Configuration({
- basePath:
-  PlaidEnvironments[process.env.PLAID_ENV as keyof typeof PlaidEnvironments],
- baseOptions: {
-  headers: {
-   "PLAID-CLIENT-ID": process.env.PLAID_CLIENT_ID || "",
-   "PLAID-SECRET": process.env.PLAID_SECRET || "",
+  basePath:
+    PlaidEnvironments[process.env.PLAID_ENV as keyof typeof PlaidEnvironments],
+  baseOptions: {
+    headers: {
+      "PLAID-CLIENT-ID": process.env.PLAID_CLIENT_ID || "",
+      "PLAID-SECRET": process.env.PLAID_SECRET || "",
+    },
   },
- },
 });
 
+// singleton pattern
 class PlaidService {
- private static instance: PlaidService;
- private client: PlaidApi;
+  private static instance: PlaidService;
+  private client: PlaidApi;
 
- private constructor(config: Configuration) {
-  this.client = new PlaidApi(config);
- }
-
- static getInstance(config: Configuration): PlaidService {
-  if (!this.instance) {
-   this.instance = new PlaidService(config);
+  private constructor(config: Configuration) {
+    this.client = new PlaidApi(config);
   }
-  return this.instance;
- }
 
- getClient(): PlaidApi {
-  return this.client;
- }
+  static getInstance(config: Configuration): PlaidService {
+    if (!this.instance) {
+      this.instance = new PlaidService(config);
+    }
+    return this.instance;
+  }
+
+  getClient(): PlaidApi {
+    return this.client;
+  }
 }
 
 const plaidService = PlaidService.getInstance(config);
