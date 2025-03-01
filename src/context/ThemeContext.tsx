36374e25ef2bcbfa2e2b97633ef90ccc8e420124
Manish Suthar <manishsuthar078@gmail.com>
2025-03-01 15:40:06 +0530

    chore: structure impr

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,39 +1,39 @@
 "use client";
 import {
- Dispatch,
- createContext,
- SetStateAction,
- useState,
- useEffect,
+  Dispatch,
+  createContext,
+  SetStateAction,
+  useState,
+  useEffect,
 } from "react";
 
 interface iThemeContext {
- mode: string;
- setMode: Dispatch<SetStateAction<string>>;
+  mode: string;
+  setMode: Dispatch<SetStateAction<string>>;
 }
 
 const ThemeContext = createContext<iThemeContext | null>(null);
 
 const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
- const [mode, setMode] = useState("dark");
- const handleChangeMode = () => {
-  if (
-   localStorage.theme === "dark" ||
-   (!("theme" in localStorage) &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches)
-  ) {
-   setMode("dark");
-   document.documentElement.classList.add("dark");
-  } else {
-   setMode("light");
-   document.documentElement.classList.remove("dark");
-  }
- };
- useEffect(() => {
-  handleChangeMode();
- }, [mode]);
+  const [mode, setMode] = useState("dark");
+  const handleChangeMode = () => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      setMode("dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.remove("dark");
+    }
+  };
+  useEffect(() => {
+    handleChangeMode();
+  }, [mode]);
 
- return <ThemeContext value={{ mode, setMode }}>{children}</ThemeContext>;
+  return <ThemeContext value={{ mode, setMode }}>{children}</ThemeContext>;
 };
 
 export { ThemeProvider, ThemeContext };
